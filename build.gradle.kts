16a85296ce37c0c54ab59e0e20bd1732e4759557
kgasss05-bit <k.gasss.05@gmail.com>
2025-09-14 15:52:23 +0200

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,18 @@
+
 plugins {
-    id("java")
-    id("net.labymod.gradle-addon") version "0.4.27"
+    id("net.labymod.gradle-addon") version "0.5.1"
 }
 
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-}
+group = "org.ulgu.autosubtitle"
+version = "1.0.0"
 
 labyMod {
     defaultPackageName = "org.ulgu.autosubtitle"
     addonInfo {
         namespace = "autosubtitle"
-        displayName = "AutoSubtitle"
+        displayName = "Auto Subtitle Addon"
         author = "Villager_Horst"
-        description = "Automatically manages subtitle timing and settings."
+        description = "Shows a countdown timer like 1y 7M 8d 23h 34m 59s left."
         minecraftVersion = "1.21.5"
     }
 }

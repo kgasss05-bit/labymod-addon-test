67b484fc8aa208fb77f1aca91af98a28908e181e
kgasss05-bit <k.gasss.05@gmail.com>
2025-09-14 16:02:24 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
-
 plugins {
-    id("net.labymod.gradle-addon") version "0.5.1"
+    id("java")
 }
 
-group = "org.ulgu.autosubtitle"
-version = "1.0.0"
+allprojects {
+    repositories {
+        mavenCentral()
+        maven("https://maven.labymod.net/releases")
+    }
+}
 
-labyMod {
-    defaultPackageName = "org.ulgu.autosubtitle"
-    addonInfo {
-        namespace = "autosubtitle"
-        displayName = "Auto Subtitle Addon"
-        author = "Villager_Horst"
-        description = "Shows a countdown timer like 1y 7M 8d 23h 34m 59s left."
-        minecraftVersion = "1.21.5"
+subprojects {
+    apply(plugin = "java")
+
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
 }
